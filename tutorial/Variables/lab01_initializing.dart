2d688afa6725472d80779477fbf23067cfa85e74
tranvanhieu21032001 <tranvanhieu21032001@gmail.com>
2025-09-04 11:31:27 +0700

    first commit

---
+++ tutorial/Variables/lab01_initializing.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  //   String? name  // Nullable type. Can be `null` or string.
+  // String name   // Non-nullable type. Cannot be `null` but can be string.
+
+  var name = 'Bod';
+  print("name: $name");
+
+  Object name2 = 'Vanhieu';
+  print("name2: $name2");
+
+  String? name3 = 'Bob';
+  print("name3: $name3");
+}
